280e184a3b770ade6986430e025f41ca75dceebb
raulandrade <isc.raulandrade@gmail.com>
2023-03-29 19:32:03 -0600

    se agrego un tercer mensaje

--- src/com/rulo/Main.java
+++ src/com/rulo/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!\n"+"Hola Mundo!!");
         System.out.println("Adios mundo");
+        System.out.println("Commit para la rama dev :v");
     }
 }
