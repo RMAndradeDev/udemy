f89ef682c7f00da15de86a06ff7a696e94e3cc71
raulandrade <isc.raulandrade@gmail.com>
2023-03-29 19:27:37 -0600

    commit por el segundo sout

--- src/com/rulo/Main.java
+++ src/com/rulo/Main.java
@@ -3,5 +3,6 @@ package com.rulo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!\n"+"Hola Mundo!!");
+        System.out.println("Adios mundo");
     }
 }
