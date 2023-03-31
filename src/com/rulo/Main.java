0870f967834cafcfcc2e037b7c966486cd385935
raulandrade <isc.raulandrade@gmail.com>
2023-03-31 15:57:45 -0600

    creacion de la clase test no coverage

--- src/com/rulo/Main.java
+++ src/com/rulo/Main.java
@@ -1,7 +1,37 @@
 package com.rulo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //tutorial();
+        test();
+    }
+
+    private static void test() {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+
+        for (int row=1;row<=n;row++){
+            //System.out.println(row);
+            System.out.printf("%n%d%n", row);
+            for (int col=row;col<2*n;col++){
+                System.out.printf("%d",col);
+            }
+        }
+        System.out.println("\n\n\n\n\n");
+
+        for (int row=1;row<=n;row++){
+            //System.out.println(row);
+            System.out.printf("%d", row);
+            for (int col=row;col<2*n;col++){
+                System.out.printf("%d",col);
+            }
+        }
+    }
+
+    public static void tutorial(){
+        System.out.printf("----------%n");
         System.out.println("Hello world!!\n"+"Hola Mundo!!");
         System.out.println("Adios mundo");
         System.out.println("Commit para la rama dev :v");
